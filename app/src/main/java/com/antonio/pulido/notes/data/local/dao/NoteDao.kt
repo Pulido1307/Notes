7a7d5150fb010ca5296073d9c4083ccb08ca7f8a
Antonio Pulido <antonio.pulidovirrueta@outlook.com>
2025-08-07 12:13:46 -0600

    feat: Add configure for Dagger Hilt and Room

---
+++ app/src/main/java/com/antonio/pulido/notes/data/local/dao/NoteDao.kt
@@ -0,0 +1,24 @@
+package com.antonio.pulido.notes.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.antonio.pulido.notes.data.model.NoteEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoteDao {
+    @Insert
+    suspend fun addNote(noteEntity: NoteEntity)
+
+    @Update
+    suspend fun updateNote(noteEntity: NoteEntity)
+
+    @Delete
+    suspend fun deleteNote(noteEntity: NoteEntity)
+
+    @Query("SELECT * FROM notes ORDER BY id")
+    fun getNotes(): Flow<List<NoteEntity>>
+}
