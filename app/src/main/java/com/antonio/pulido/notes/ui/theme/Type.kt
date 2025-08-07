ac1d07bca6c1054ee85b0cd753d2ffb4517b25a6
Antonio Pulido <antonio.pulidovirrueta@outlook.com>
2025-08-07 12:18:55 -0600

    feat: Add font for the texts

--- app/src/main/java/com/antonio/pulido/notes/ui/theme/Type.kt
+++ app/src/main/java/com/antonio/pulido/notes/ui/theme/Type.kt
@@ -2,10 +2,17 @@ package com.antonio.pulido.notes.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.antonio.pulido.notes.R
 
+val EcoWalletFont = FontFamily(
+    Font(R.font.rubik_bold, FontWeight.Bold),
+    Font(R.font.rubik_medium, FontWeight.Medium),
+    Font(R.font.rubik_regular, FontWeight.Normal)
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
